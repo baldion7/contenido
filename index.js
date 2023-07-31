b0539fc730bf8077835243057078ecf0b6e20a84
baldi <baldionkevin8@gmail.com>
2023-07-31 15:38:40 -0500

    carpetas

--- index.js
+++ index.js
@@ -22,12 +22,6 @@ app.use('public', express.static(new URL('./public', import.meta.url).pathname))
 app.use(express.static('public', { extensions: ['html', 'css','js'], }));
 
 
-
-app.use((req, res, next) => {
-    res.status(404).redirect('/');
-});
-
-
 app.listen(process.env.APP_PORT,()=>{
     console.log('prendio')
 });
